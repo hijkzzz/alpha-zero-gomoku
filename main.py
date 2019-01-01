6b5f9b7e945a3acacadf4d9c9335c87478c1d7b5
hijkzzz <janhu9527@gmail.com>
2019-01-01 18:38:24 +0800

    modify

--- main.py
+++ main.py
@@ -13,20 +13,20 @@ args = dotdict({
     'nir': 3,
 
     'num_iters': 1000,
-    'num_eps': 200,
-    'explore_num' : 7,
+    'num_eps': 150,
+    'explore_num' : 5,
     'dirichlet_alpha' : 0.3,
     'update_threshold': 0.55,
     'area_num': 20,
     'temp_examples_max_len': 100000,
     'train_examples_max_len': 5,
 
-    'num_mcts_sims': 100, 
-    'cpuct': 10,
+    'num_mcts_sims': 150, 
+    'cpuct': 5,
 
     'lr': 0.001,
     'l2': 0.0001,
-    'epochs': 5,
+    'epochs': 3,
     'batch_size': 64,
     'num_channels': 128,
 
@@ -45,4 +45,5 @@ if __name__ == "__main__":
     if not args.human_play:
         alpha_zero.learn()
     else:
-        alpha_zero.human_play()
+        print(alpha_zero.human_play())
+        raw_input()
