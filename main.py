e0b1b2c3e661734270f6b42ec16f78834b364063
janhu <janhu9527@gmal.com>
2019-01-04 18:52:54 +0800

    zzz

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ args = dotdict({
 
     'num_iters': 1000,
     'num_eps': 10,
-    'explore_num' : 2,
+    'explore_num' : 4,
     'temp' : 5,
     'dirichlet_alpha' : 0.3,
     'update_threshold': 0.55,
-    'area_num': 4,
+    'area_num': 6,
     'temp_examples_max_len': 10000,
     'train_examples_max_len': 100,
 
