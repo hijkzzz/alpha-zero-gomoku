32f45f4d62d45ab14ea1ac6b3fd6fe48953d8b82
hijkzzz <janhu9527@gmail.com>
2019-01-04 08:51:40 +0800

    zzz

--- alpha_zero/alpha_zero.py
+++ alpha_zero/alpha_zero.py
@@ -115,9 +115,9 @@ class AlphaZero():
             pi, counts = mcts.get_action_prob(canonical_board, temp=temp)
 
             # Dirichlet noise
-            pi = 0.75 * np.array(pi) + \
-                0.25 * np.random.dirichlet(self.args.dirichlet_alpha * np.ones(len(pi))) * (np.array(counts) > 0)
-            pi = pi / np.sum(pi) # renormalize
+            # pi = 0.75 * np.array(pi) + \
+                # 0.25 * np.random.dirichlet(self.args.dirichlet_alpha * np.ones(len(pi))) * (np.array(counts) > 0)
+            # pi = pi / np.sum(pi) # renormalize
 
             sym = self.game.get_symmetries(canonical_board, pi)
             for b, p in sym:
