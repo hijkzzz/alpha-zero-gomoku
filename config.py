c398d3b3eeff1d52acda6746f94eba241cc4fedf
hijkzzz <janhu9527@gmail.com>
2019-01-24 15:12:31 +0800

    need debug

---
+++ config.py
@@ -0,0 +1,33 @@
+config = {
+    # gomoku
+    'n': 10,
+    'n_in_row': 5,
+
+    # mcts
+    'thread_pool_size': 4,
+    'num_mcts_sims': 400,
+    'c_puct': 5,
+    'c_virtual_loss': 0.1,
+
+    # neural_network
+    'lr': 0.002,
+    'l2': 0.0002,
+    'num_channels': 128,
+    'epochs': 5,
+    'batch_size': 512,
+    'kl_targ': 0.04,
+
+    # train
+    'num_iters': 100000,
+    'num_eps': 1,
+    'explore_num': 2,
+    'temp': 1,
+    'dirichlet_alpha': 0.3,
+    'update_threshold': 0.55,
+    'contest_num': 10,
+    'check_freq': 50,
+    'examples_buffer_max_len': 10000,
+
+    # test
+    'human_color': 1
+}
--- test/leaner_test.py
+++ test/leaner_test.py
@@ -2,13 +2,11 @@
 import sys
 sys.path.append('..')
 sys.path.append('../src')
-import configparser
 
 import learner
+import config
 
 if __name__ == "__main__":
-    config = configparser.ConfigParser()
-    config.read('config.ini')
 
-    alpha_zero = learner.Leaner(config)
+    alpha_zero = learner.Leaner(config.config)
     alpha_zero.learn()
