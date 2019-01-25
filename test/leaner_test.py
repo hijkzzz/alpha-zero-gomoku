5f399c1e7be7a498b06aa0c81d25a89a9a370ac9
janhu <janhu9527@gmal.com>
2019-01-25 12:23:17 +0800

    add human play

--- test/leaner_test.py
+++ test/leaner_test.py
@@ -12,5 +12,16 @@ if __name__ == "__main__":
     # print(learner.tuple_2d_to_numpy_2d(((2, 3, 0.1, 0.5, 0), (2, 3, 0.1, 0.5, 0),
                                         # (2, 3, 0.1, 0.5, 0), (2, 3, 0.1, 0.5, 0), (2, 3, 0.1, 0.5, 0))))
 
+    if len(sys.argv) < 2:
+        print("param error: train or play")
+        exit(1)
+
     alpha_zero = learner.Leaner(config.config)
-    alpha_zero.learn()
+
+    if sys.argv[1] == "train":
+        alpha_zero.learn()
+    elif sys.argv[1] == "play":
+        alpha_zero.play_with_human(human_first=True)
+    else:
+        print("param error: train or play")
+        exit(1)
