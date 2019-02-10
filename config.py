473bb52c721f1a45bf3b95da366b44e1a0b478bc
hijkzzz <janhu9527@gmail.com>
2019-02-10 18:04:41 +0800

    zzz

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ config = {
     'lr': 0.001,                                # learning rate
     'l2': 0.0001,                               # L2
     'num_channels': 128,                        # convolution neural network channel size
-    'epochs': 5,                                # train epochs
+    'epochs': 2,                                # train epochs
     'batch_size': 512,                          # batch size
     'kl_targ': 0.01,                            # threshold of KL divergence
 
