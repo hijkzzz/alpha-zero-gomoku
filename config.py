eb076d25e3643a0f4a538c4b9974f8176b25f44b
hijkzzz <janhu9527@gmail.com>
2019-02-17 13:47:06 +0800

    zzz

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ config = {
     'mcts_use_gpu' : True,                      # mcts use cuda
     'thread_pool_size': 4,                      # mcts threads number
     'num_mcts_sims': 1600,                      # mcts simulation times
-    'c_puct': 3,                                # puct coeff
+    'c_puct': 5,                                # puct coeff
     'c_virtual_loss': 3,                        # virtual loss coeff
 
     # neural_network
