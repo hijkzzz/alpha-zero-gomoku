b336a0993b33a68c0bc0626d450fad3934cbe292
hijkzzz <janhu9527@gmail.com>
2019-03-14 22:11:14 +0800

    zzz

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ config = {
     'num_iters': 100000,                        # train iterations
     'num_eps': 10,                              # self play times in per iter
     'parallel_play_size': 10,                   # self play in parallel
-    'explore_num': 8,                           # explore step in a game
+    'explore_num': 20,                          # explore step in a game
     'temp': 1,                                  # temperature
     'dirichlet_alpha': 0.03,                    # action noise in self play games
     'update_threshold': 0.55,                   # update model threshold
